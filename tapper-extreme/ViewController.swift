2915960863a072494520702b7301cacb9dec6f4b
rtallmadge <ryan.tallmadge@cbsinteractive.com>
2015-12-05 22:10:54 -0500

    Adding tapper extreme

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,17 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //Properties
+    var maxTaps     : Int = 0;
+    var currentTaps : Int = 0;
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImgVIew      : UIImageView!
+    @IBOutlet weak var howManyTapsTxt   : UITextField!
+    @IBOutlet weak var playBtn          : UIButton!
+    @IBOutlet weak var tapBtn           : UIButton!
+    @IBOutlet weak var tapsLabel        : UILabel!
+    
+    
+    func toggleViews(showHide: Bool){
+        logoImgVIew.hidden      = showHide ? true   : false;
+        playBtn.hidden          = showHide ? true   : false;
+        howManyTapsTxt.hidden   = showHide ? true   : false;
+        tapBtn.hidden           = showHide ? false  : true;
+        tapsLabel.hidden        = showHide ? false  : true;
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTapsLabel(taps : Int){
+        tapsLabel.text = "\(taps) Taps";
     }
-
-
+    
+    func isGameOver(taps : Int) -> Bool{
+        if(taps >= maxTaps){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    func restartGame(){
+        maxTaps     = 0;
+        currentTaps = 0;
+        toggleViews(false);
+    }
+    
+    @IBAction func onCoinTap(sender: AnyObject){
+        currentTaps++;
+        if(isGameOver(currentTaps)){
+            restartGame();
+        }else{
+            updateTapsLabel(currentTaps);
+        }
+    }
+    
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!){
+        
+        if(howManyTapsTxt.text != nil && howManyTapsTxt.text != ""){
+            maxTaps     = Int(howManyTapsTxt.text!)!;
+            currentTaps = 0;
+            toggleViews(true);
+            updateTapsLabel(currentTaps);
+        }
+        
+    }
+    
+    
 }
 
